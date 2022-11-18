504401b01e47c2166aa6cf6c92095fe3a8724b6b
mohan-mohe <mohanrajmohe031100@gmail.com>
2022-11-19 00:38:43 +0530

    updated the app.py

--- Project Development Phase/Sprint -4/integration.py
+++ Project Development Phase/Sprint -4/integration.py
@@ -1,14 +1,12 @@
 #importing required libraries
 
-import pickle
-import warnings
-
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
+from sklearn import metrics 
+import warnings
+import pickle
 import requests
-from flask import Flask, render_template, request
-from sklearn import metrics
-
 warnings.filterwarnings('ignore')
 from feature import FeatureExtraction
 
@@ -17,12 +15,13 @@ gbc = pickle.load(file)
 file.close()
 
 # NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
-API_KEY = "cWGD5yTjEpEGtqPpvHPDBElN5eXFS7eh2JRDyUWhySMW"
+API_KEY = "H_eQnWI4923bdKM63m1wH9G07eo-uRpUYs9b7FqNPCVF"
 token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
  API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
 mltoken = token_response.json()["access_token"]
 
 header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
 
 app = Flask(__name__)
 
@@ -43,7 +42,7 @@ def index():
         pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
         payload_scoring = {"input_data": [{"field": [["UsingIP","LongURL","ShortURL","Symbol@","Redirecting//","PrefixSuffix-","SubDomains","HTTPS","DomainRegLen","Favicon","NonStdPort","HTTPSDomainURL","RequestURL","AnchorURL","LinksInScriptTags","ServerFormHandler","InfoEmail","AbnormalURL","WebsiteForwarding","StatusBarCust","DisableRightClick","UsingPopupWindow","IframeRedirection","AgeofDomain","DNSRecording","WebsiteTraffic","PageRank","GoogleIndex","LinksPointingToPage","StatsReport"
 ]], "values": [[1,1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,1,1,1,0,1,1,1,1,-1,-1,-1,-1,1,0,1]]}]}
-        response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/084b5c52-f617-40ef-a0e8-3e6cf79ae447/predictions?version=2022-11-06', json=payload_scoring,
+        response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/73df506a-fc05-42ee-848a-ee6d13e9c4ac/predictions?version=2022-11-17', json=payload_scoring,
         headers={'Authorization': 'Bearer ' + mltoken})
         print("Scoring response")
         predictions=response_scoring.json()
